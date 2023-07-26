a7566e884a39554a84eacede8bbde67e4368774a
Vincent Haryadi <vincenthary19@gmail.com>
2023-07-27 01:16:33 +0700

    chore: testing lint-stage

--- app/(tabs)/forms/simple.tsx
+++ app/(tabs)/forms/simple.tsx
@@ -11,6 +11,7 @@ const SimpleForm = () => {
   return (
     <SafeAreaView>
       <Text>Hello Simple Form</Text>
+      <Text>New Text linter</Text>
       <STextInput placeholder="hello" />
     </SafeAreaView>
   );
